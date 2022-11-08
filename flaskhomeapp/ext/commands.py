e6996b26ec76f822c7d502db6966df31ed8a6a07
NiranjanW <NiranjanW@users.noreply.github.com>
2022-11-08 12:35:21 +0000

    ✅ Ready to clone and code.

--- flaskhomeapp/ext/commands.py
+++ flaskhomeapp/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from flaskhomeapp.ext.auth import create_user
+from flaskhomeapp.ext.database import db
+from flaskhomeapp.models import Product
 
 
 def create_db():
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from flaskhomeapp import create_app
+from flaskhomeapp.ext.commands import populate_db
+from flaskhomeapp.ext.database import db
 
 
 @pytest.fixture(scope="session")
